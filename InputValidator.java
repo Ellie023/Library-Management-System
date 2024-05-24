43b2a489b5ad0323edc5dc566a6a2fa7d162e946
Ellie <100378349+Ellie023@users.noreply.github.com>
2024-05-25 01:04:02 +0900

    Update InputValidator.java
    주석

--- InputValidator.java
+++ InputValidator.java
@@ -3,73 +3,33 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class InputValidator {
+public class InputValidator {//input 값 유효성 검사, table에 없으면 false return
 
-    public static boolean is_Valid_Memberid(int member_id){
+    public static boolean is_Valid_Memberid(int member_id){ //member_id가 members table에 있는 지 확인
 
         String checkSQL="SELECT member_id from Members where member_id=?";
         try (Connection con =DatabaseConnection.getConnection();
              PreparedStatement pstmt =con.prepareStatement(checkSQL)){
             pstmt.setInt(1, member_id);
             ResultSet rs = pstmt.executeQuery();
-            return rs.next(); //
+            return rs.next(); //존재하면 true return
 
         } catch (SQLException e) {
             e.printStackTrace();
-            return false;
+            return false; //없으면 false return
         }
     }
-    public static boolean is_vaild_Bookid(int book_id){
-
-        String checkSQL="SELECT book_id from Books where book_id=?";
-        try(Connection con =DatabaseConnection.getConnection();
-        PreparedStatement pstmt =con.prepareStatement(checkSQL)){
-            pstmt.setInt(1,book_id);
-            ResultSet rs =pstmt.executeQuery();
-            return rs.next();
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-            return false;
-        }
-    }
-    public static boolean is_vaild_Booktitle(String title){
+    public static boolean is_vaild_Booktitle(String title){//책 title이 books table에 있는 지 확인
         String checkSQL="SELECT title from Books where title=?";
         try(Connection con=DatabaseConnection.getConnection();
             PreparedStatement pstmt = con.prepareStatement(checkSQL)){
             pstmt.setString(1,title);
             ResultSet rs=pstmt.executeQuery();
-            return rs.next();
+            return rs.next();//존재하면 true return
         }
         catch (SQLException e){
             e.printStackTrace();
-            return false;
-        }
-    }
-    public static boolean is_vaild_name(String name){
-        String checkSQL="select name from Members where name=?";
-        try(Connection con=DatabaseConnection.getConnection();
-            PreparedStatement pstmt = con.prepareStatement(checkSQL)){
-            pstmt.setString(1,name);
-            ResultSet rs=pstmt.executeQuery();
-            return rs.next();
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-            return false;
-        }
-    }
-    public static boolean is_vaild_bookgenre(String genre){
-        String checkSQL="select genre from Books where genre=?";
-        try(Connection con=DatabaseConnection.getConnection();
-            PreparedStatement pstmt = con.prepareStatement(checkSQL)){
-            pstmt.setString(1,genre);
-            ResultSet rs=pstmt.executeQuery();
-            return rs.next();
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-            return false;
+            return false;// 졵배하지 않으면 false return
         }
     }
 }
